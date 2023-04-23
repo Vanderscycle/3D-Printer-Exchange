661ec871f95cc04fba41a905a30d5546089e8034
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-04-22 21:31:39 -0700

    able to parse answer

--- backend/testing/script.py
+++ backend/testing/script.py
@@ -6,12 +6,11 @@ import requests
 
 port: int = 8080
 baseUrl: str = f"localhost:{port}"
-dummyAcct: dict[str, str] = {"username": "borisxss",
-                   "email":"borixs@mcBoris.com",
+dummyAcct: dict[str, str] = {"username": "dasdborisxsss",
+                   "email":"bsdsdorixss@mcBoris.com",
                    "password":"nottVerySafeInit"}
 headers: dict[str, str] = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-parseResponse: str = "{'data': {'CreatedAt': '2023-04-22T20:25:34.835144066-07:00', 'UpdatedAt': '2023-04-22T20:25:34.835144066-07:00', 'DeletedAt': None, 'ID': '27b7bf49-5701-41ac-a2c2-43d2cab60745', 'username': 'borisxss', 'email': 'borixs@mcBoris.com', 'password': '$2a$14$5cOwOb8Tml3hd5y0DzzPV.lwoAkcvuhCUhycQMEAQAAdwwe2UH5bG'}, 'message': 'User has created', 'status': 'success'}"
 # convert into JSON:
 dummyAcctPayload: str = json.dumps(dummyAcct, indent=2)
 
@@ -21,7 +20,7 @@ def ping(endpoint: str) -> None:
     # os.system(f'http GET {baseUrl}/{endpoint}')
     r = requests.post(f'http://{baseUrl}/{endpoint}', json=dummyAcct, headers=headers) # needs to capture the return data
     data = r.json()
-    print(data)
+    print(data['data']['ID']) # TODO: add a try/catch block
     # os.system(f'http GET {baseUrl}/{endpoint}/1')
     # os.system(f'http DELETE {baseUrl}/{endpoint}/1')
 
