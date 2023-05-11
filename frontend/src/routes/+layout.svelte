606d48a2d8eebf43aa23ae5a0d58228134d68cd4
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-05-10 18:45:59 -0700

    added themes + dark mode

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../app.css';
+	import Modal from 'svelte-simple-modal';
 </script>
 
-<slot />
+<main class="dark:bg-darkBg bg-Bg h-screen">
+	<Modal>
+		<slot />
+	</Modal>
+</main>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+</style>
