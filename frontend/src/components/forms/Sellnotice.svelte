22be9fd369412e1830580e9eaaef714340dcb688
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2023-05-14 09:00:40 -0700

    test form working will modify to fit

--- frontend/src/components/forms/Sellnotice.svelte
+++ frontend/src/components/forms/Sellnotice.svelte
@@ -1,6 +1,6 @@
 <script>
-	// import classNames from "vest/classNames";
- // import validate from './validate';
+	import classnames from 'vest/classnames';
+	import validate from './validate';
 
 	import Input from '$components/ui/Input.svelte';
 	import Button from '$components/ui/Button.svelte';
@@ -15,80 +15,84 @@
 	// this is a good practice and helps us with resetting the form
 	let formState = { ...values };
 
-	let usernamePending = false;
-
 	// initial empty validation state
- // let result = validate.get();
- //
- // 	const check = ({ target: { name } }) => runValidate(name);
- //
- // 	const runValidate = (name) => {
- // 		result = validate(formState, name);
- //
- // 		// extract errors and warnings
- // 		errors = result.getErrors();
- // 		warnings = result.getWarnings();
- //
- // 	};
- //
- // const reset = () => {
- // 		validate.reset();
- // 		result = validate.get();
- // 		errors = {};
- // 		warnings = {};
- // 		formState = { ...values };
- // 	};
- //
- // 	const onSubmit = () => {
- // 		runValidate();
- // 		if (result.hasErrors()) return;
- // 		submit(formState);
- // 		// reset form state
- // 		reset();
- // 	};
- //
+	let result = validate.get();
+
+	const check = ({ target: { name } }) => runValidate(name);
+
+	const runValidate = (name) => {
+		result = validate(formState, name);
+
+		// extract errors and warnings
+		errors = result.getErrors();
+		warnings = result.getWarnings();
+	};
+
+	const reset = () => {
+		validate.reset();
+		result = validate.get();
+		errors = {};
+		warnings = {};
+		formState = { ...values };
+	};
+
+	const onSubmit = () => {
+		runValidate();
+		if (result.hasErrors()) return;
+		submit(formState);
+		// reset form state
+		reset();
+	};
+
 	// Vest css class helper utility
-	// $: cn = classNames(result, {
-	//   warning: "warning",
-	//   invalid: "invalid",
-	//   valid: "valid"
-	// });
+	$: cn = classnames(result, {
+		warning: 'warning',
+		invalid: 'invalid',
+		valid: 'valid'
+	});
 	//
 	// reactive variable for the submit button
- // $: disabled = result.hasErrors();
+	$: disabled = result.hasErrors();
 </script>
 
-<form id="example-form" class="col-xs-10 col-lg-6">
+<form on:submit|preventDefault={onSubmit} id="example-form" class="col-xs-10 col-lg-6">
 	<h2>Authentication</h2>
 	<Input
 		name="username"
 		label="Username"
-		pending={usernamePending}
+		class={cn('username')}
 		placeholder="try: ealush or codechips"
 		{errors}
 		bind:value={formState.username}
+		onInput={check}
 	/>
 	<Input
 		name="password"
 		label="Password"
+		class={cn('password')}
 		errors={{ ...errors, ...warnings }}
 		bind:value={formState.password}
+		onInput={check}
 	/>
 	<Input
 		name="confirm_password"
 		label="Confirm Password"
+		class={cn('confirm_password')}
 		{errors}
 		bind:value={formState.confirm_password}
+		onInput={check}
 	/>
-	<Input name="email" label="Email" {errors} bind:value={formState.email}/>
+	<Input name="email" label="Email" {errors} bind:value={formState.email} />
+	class={cn('email')}
 	<Checkbox
 		name="tos"
 		label="I have read and agreed to the terms of service."
+		class={cn('tos')}
 		bind:checked={formState.tos}
+		onInput={check}
 	/>
 	<footer>
-		<Button  >Submit</Button>
+		{disabled}
+		<Button class="btn-submit" type="submit" {disabled}>Submit</Button>
 	</footer>
 </form>
-
-	<!-- onInput={check} -->
--- frontend/src/components/forms/validate.js
+++ frontend/src/components/forms/validate.js
@@ -1,13 +1,13 @@
-import vest, { test, enforce } from 'vest';
+import { create, only, warn, test, enforce } from 'vest';
 import { isEmail } from 'validator';
 
 // https://codesandbox.io/s/svelte-vest-form-validation-example-kf185?file=/validate.js:0-1387
 // extend vest with customer email validator
 enforce.extend({ isEmail });
 
-const suite = vest.create('user_form', (data = {}, currentField) => {
+const suite = create('user_form', (data = {}, currentField) => {
 	// if field name is supply validate only that field
-	vest.only(currentField);
+	only(currentField);
 
 	test('username', 'Username is required', () => {
 		enforce(data.username).isNotEmpty();
@@ -23,7 +23,7 @@ const suite = vest.create('user_form', (data = {}, currentField) => {
 
 	test('password', 'Password is weak, Maybe add a number?', () => {
 		// mark this test as a warning only
-		vest.warn();
+		warn();
 		enforce(data.password).matches(/[0-9]/);
 	});
 
