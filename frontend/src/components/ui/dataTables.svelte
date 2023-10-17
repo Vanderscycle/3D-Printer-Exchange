9f1b0e57e831678ea44fb7b59480547423946312
vanderscycle <henri-vandersleyen@protonmail.com>
2023-10-17 16:50:25 -0700

    prettier table + api call to create/show user

--- frontend/src/components/ui/dataTables.svelte
+++ frontend/src/components/ui/dataTables.svelte
@@ -1,24 +1,27 @@
 <script>
     export let data = [];
+//     let columns = data.length > 0 ? Object.keys(data[0]) : [];
+    $: columns = data.length > 0 ? Object.keys(data[0]) : [];
 
-    let columns = data.length > 0 ? Object.keys(data[0]) : [];
 </script>
 
-<table border="1">
-    <thead>
-        <tr>
-            {#each columns as column}
-                <th>{column}</th>
-            {/each}
-        </tr>
-    </thead>
-    <tbody>
-        {#each data as row}
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 {#each columns as column}
-                    <td>{row[column]}</td>
+                    <th scope="col" class="px-6 py-3">{column}</th>
                 {/each}
             </tr>
-        {/each}
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            {#each data as row}
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    {#each columns as column}
+                        <td>{row[column]}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
--- frontend/src/routes/localhost/+page.svelte
+++ frontend/src/routes/localhost/+page.svelte
@@ -4,12 +4,27 @@ import Button from "$components/ui/Button.svelte";
 import type {Printer} from "$lib/interfaces"
 import DataTables from "$components/ui/dataTables.svelte";
 import { onMount } from "svelte";
+
 let api = new GoRestClient(true)
 
+let printersTable;
+let usersTable;
+
+onMount(() => {
+    // Make the API call right when the component mounts
+    getAllPrinter();
+});
+
+// live check
 let healthCheckPromise;
+
+function healthCheck() {
+    healthCheckPromise = api.get("ancillary/version/");
+}
+
+//printer
 let postPrinterPromise;
 let getAllPrinterPromise;
-
 let printerTestPayload: Printer = {
     user: "bob",
     brand: "Ratrig",
@@ -24,14 +39,6 @@ let printerTestPayload: Printer = {
     mods: "nil"
 }
 
-onMount(() => {
-    // Make the API call right when the component mounts
-    getAllPrinter();
-});
-
-function healthCheck() {
-    heatlhCheckPromise = api.get("ancillary/version/");
-}
 
 function postPrinter() {
     postPrinterPromise = api.post("api/printer", printerTestPayload);
@@ -40,37 +47,63 @@ function postPrinter() {
 function getAllPrinter() {
     getAllPrinterPromise = api.get("api/printer");
 }
+//TODO: add the option, when clicking on a row, to fetch that data and show clearly
 
+//user
+let postNewUserPromise;
+let getAllUserPromise;
+let userTestPayload: any = {
+    username: "bob",
+    email: "bob@printer.com",
+    password: "superDooberSecretLMAO"
+}
+function postNewUser() {
+    postPrinterPromise = api.post("api/user", userTestPayload);
+}
 
+function getAllUser() {
+    getAllPrinterPromise = api.get("api/user");
+}
 </script>
 
-
-<Button callbackFn={() => console.log("hello there")}>test</Button>
 
 <Button callbackFn={() => healthCheck()}>Get api version</Button>
 <Button callbackFn={() => postPrinter()}>Add default printer</Button>
 <Button callbackFn={() => getAllPrinter()}>Refresh printer list</Button>
 
+<Button callbackFn={() => postNewUser()}>Add default user</Button>
+<Button callbackFn={() => getAllUser()}>Refresh user list</Button>
+
 <!-- <p>Use the onMount and whatnot</p> -->
 
-{#await healthCheckPromise}
-    <p>...waiting</p>
-{:then status}
-    {#if status === null || status === undefined}
-        <p>Status is unavailable.</p>
-    {:else}
-        {#each Object.entries(status) as [key, value]}
+{#if healthCheckPromise}
+    {#await healthCheckPromise then health}
+        {#each Object.entries(health) as [key, value]}
             <p>{key}: {value}</p>
         {/each}
-    {/if}
-{:catch error}
-    <p style="color: red">{error.message}</p>
-{/await}
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+{:else}
+    <p>...waiting</p>
+{/if}
+
 
 {#if getAllPrinterPromise}
     {#await getAllPrinterPromise then printers}
-        <DataTables data={printers.data} />
+        <DataTables data={printers.data}/>
     {:catch error}
         <p style="color: red">{error.message}</p>
     {/await}
 {/if}
+
+<!-- it reuses the same table as above -->
+{#if getAllUserPromise}
+    {#await getAllUserPromise then users}
+        <DataTables data={users.data}/>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+{/if}
+
+<hr class="border-t-2 border-red-500 w-1/2 mx-auto my-8">
