2f353e21c52bd071412657d04d3084e0336eebf2
vanderscycle <henri-vandersleyen@protonmail.com>
2023-11-17 19:40:54 -0800

    reworked about + played with slugs in user

--- frontend/src/routes/user/[slug]/+page.js
+++ frontend/src/routes/user/[slug]/+page.js
@@ -1,5 +1,8 @@
-export const load = ({ params }) => {
-	return {
-		slug: params.slug
-	};
-};
+import { fetchUserByUsername } from '$lib/api/userData';
+
+export async function load({ params }) {
+  const username  = params.slug;
+  console.log('username', username);
+  const user = await fetchUserByUsername(username);
+  return { props: { user } };
+}
--- frontend/src/routes/user/[slug]/+page.svelte
+++ frontend/src/routes/user/[slug]/+page.svelte
@@ -1,5 +1,21 @@
 <script>
-	export let data;
+export let data;
+console.log(data);
+let user = data.props.user
+if (!user) {
+    user = { username: 'Not found', email: 'N/A', bio: 'User does not exist.' };
+}
 </script>
 
-<h1>{data.slug}</h1>
+<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 class="text-2xl font-bold mb-2">User Profile</h1>
+        <p><strong>Username:</strong> {user.username}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        <p><strong>Bio:</strong> {user.bio}</p>
+    </div>
+</div>
+
+<style>
+/* Additional styling if needed */
+</style>
